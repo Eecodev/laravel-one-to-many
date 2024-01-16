bc1de84e6b13590b8fde39aef454ac0133b6598e
Eecodev <mnasser.bbk@gmail.com>
2024-01-16 22:26:57 +0100

    project index updated

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,33 +2,41 @@
 @section('content')
     <section class="container">
         <h1>Project List</h1>
-            <table class="table">
+        <div class="text-end">
+            <a href="{{route('admin.projects.create')}}" class="btn btn-success">Create new project</a>
+        </div>
+        @if(session()->has('message'))
+            <div class="alert alert-success mb-3 mt-3">
+                {{session(-get('message'))}}
+            </div>
+        @endif
+
+            <table class="table table-striped">
                 <thead>
                   <tr>
                     <th scope="col">Project Name</th>
-                    <th scope="col">Image</th>
                     <th scope="col">Description</th>
-                    <th scope="col">Slug</th>
+                    {{-- <th scope="col">Slug</th> --}}
                     <th scope="col">Url</th>
+                    <th scope="col">Edit</th>
+                    <th scope="col">Delete</th>
                   </tr>
                 </thead>
                 <tbody>
                     @foreach ($projects as $project)
                         <tr>
-                            <td>{{$project->title}}</td>
-                            <td>{{$project->image}}</td>
-                            <td>{{$project->description}}</td>
-                            <td>{{$project->slug}}</td>
+                            <th scope="row">{{$project->id}}</th>
+                            <td><a href="{{route('admin.projects.show', $project->slug)}}" title="View Project">{{$project->title}}</a></td>
+                            <td>{{Str::limit($project->description, 100)}}</td>
+                            <td><a href="{{route('admin.projects.show')}}">{{$project->url}}</a></td>
+                            <td><a class="link-secondary" href="{{route('admin.projects.edit', $project->slug)}}" title="Edit Project"><i class="fa-solid fa-pen"></i></a></td>
+                            
                             <td>
-                                {{$project->url}}
-                                <div class="d-flex">
-                                    <a href="{{route('admin.projects.show', $project->id)}}" class="btn btn-info border">Info</a>
-                                    <form action="{{route('admin.projects.destroy', $project->id)}}">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="cancel-btn btn btn-danger ms-3" data-item-title="{{$project->title}}">Delete</button>
-                                    </form>
-                                </div>
+                                <form action="{{route('admin.projects.destroy', $project->slug)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="delete-btn btn btn-danger ms-3" data-item-title="{{$project->title}}"><i class="fa-solid fa-trash-can"></i></button>
+                                </form>
                             </td>
                         </tr>
                     @endforeach
